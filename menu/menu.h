ad91fdf3f2c6e448f89411b6e549624306da5e0d
alv2017 <v.alishauskaite@gmail.com>
2020-07-05 22:27:08 +0100

    Menu updated to support a new delete_n_node functionality

--- menu/menu.h
+++ menu/menu.h
@@ -4,6 +4,7 @@
 void display_menu(const char **menu);
 short get_menu_length(const char **menu);
 short select_option(short minopt, short maxopt);
-int enter_node_value();
+int enter_node_value(short *op_status);
+long enter_node_number(short *op_status);
 
 #endif
