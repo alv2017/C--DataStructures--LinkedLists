0a0dc8f0a622ce131c7691cd62f7aa06f7c393f6
alv2017 <v.alishauskaite@gmail.com>
2020-07-05 22:21:01 +0100

    The function delete_node_n created and added to header file

--- src/linked_list.h
+++ src/linked_list.h
@@ -30,6 +30,8 @@ struct node *delete_node(struct node *start, int node_val, short *op_status);
 struct node *delete_node_before(struct node *start, int before_val, short *op_status);
 // Delete node after the node with a given value
 struct node *delete_node_after(struct node *start, int after_val, short *op_status);
+// Delete n-th node
+struct node *delete_node_n(struct node *start, long n, short *op_status);
 
 // Delete entire list
 struct node *delete_list(struct node *start, short *op_status);
